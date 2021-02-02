2648a8104100bb69b740e51a6e4ffb6b639d91a7
Tidbits JS <tidbitsjs@gmail.com>
2021-02-02 18:18:30 +0530

    3d object render

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ThreeScene from "./component/ThreeScene";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        display: "felx",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      {/* <h1>React with ThreeJS</h1>
+      <h2>Start editing to see some magic happen!</h2> */}
+
+      <ThreeScene />
     </div>
   );
 }
---
+++ src/component/ThreeScene.js
@@ -0,0 +1,142 @@
+import React, { Component } from "react";
+import * as THREE from "three";
+import { MTLLoader, OBJLoader } from "three-obj-mtl-loader";
+import OrbitControls from "three-orbitcontrols";
+
+class ThreeScene extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { width: 1000, height: 650 };
+  }
+
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener("resize", this.updateWindowDimensions);
+
+    const width = this.mount.clientWidth;
+    const height = this.mount.clientHeight;
+    this.scene = new THREE.Scene();
+
+    this.renderer = new THREE.WebGL1Renderer({ antialias: true });
+    this.renderer.setClearColor("#263238");
+    this.renderer.setSize(width, height);
+    this.mount.appendChild(this.renderer.domElement);
+
+    this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+    this.camera.position.z = 20;
+    this.camera.position.y = 5;
+
+    const controls = new OrbitControls(this.camera, this.renderer.domElement);
+
+    var lights = [];
+    lights[0] = new THREE.PointLight(0x304ffe, 1, 0);
+    lights[1] = new THREE.PointLight(0xffffff, 1, 0);
+    lights[2] = new THREE.PointLight(0xffffff, 1, 0);
+    lights[0].position.set(0, 200, 0);
+    lights[1].position.set(100, 200, 100);
+    lights[2].position.set(-100, -200, -100);
+    this.scene.add(lights[0]);
+    this.scene.add(lights[1]);
+    this.scene.add(lights[2]);
+
+    this.addModels();
+
+    this.renderScene();
+
+    this.start();
+  }
+
+  updateWindowDimensions = () => {
+    console.log("update window");
+    this.setState({ width: window.innerWidth, height: window.innerHeight });
+  };
+
+  addModels() {
+    const geometry = new THREE.SphereGeometry(5, 32, 32);
+    const material = new THREE.MeshBasicMaterial({ color: "#fff" });
+    this.cube = new THREE.Mesh(geometry, material);
+    this.scene.add(this.cube);
+
+    this.cube.position.y = -2;
+
+    new THREE.TextureLoader().load(
+      "./assets/freedom.jpeg",
+      (texture) => {
+        this.cube.material.map = texture;
+        this.cube.material.needsUpdate = true;
+      },
+      (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+      },
+      (error) => {
+        console.log("An error happened" + error);
+      }
+    );
+
+    var mtlLoader = new MTLLoader();
+    mtlLoader.setBaseUrl("./assets/");
+    mtlLoader.load("freedom.mtl", (materials) => {
+      materials.preload();
+      console.log("Material loaded");
+
+      var objLoader = new OBJLoader();
+      objLoader.setMaterials(materials);
+      objLoader.load(
+        "./assets/freedom.obj",
+        (object) => {
+          this.freedomMesh = object;
+          this.freedomMesh.position.setY(3);
+          this.freedomMesh.scale.set(0.02, 0.02, 0.02);
+          this.scene.add(this.freedomMesh);
+        },
+        (xhr) => {
+          console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+        },
+        (error) => {
+          console.log("An error happened" + error);
+        }
+      );
+    });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateWindowDimensions);
+    this.stop();
+    this.mount.removeChild(this.renderer.domElement);
+  }
+
+  start = () => {
+    if (!this.frameId) {
+      this.frameId = requestAnimationFrame(this.animate);
+    }
+  };
+
+  stop = () => {
+    cancelAnimationFrame(this.frameId);
+  };
+
+  animate = () => {
+    if (this.cube) this.cube.rotation.y += 0.01;
+    if (this.freedomMesh) this.freedomMesh.rotation.y += 0.01;
+
+    this.renderScene();
+    this.frameId = window.requestAnimationFrame(this.animate);
+  };
+
+  renderScene = () => {
+    if (this.renderer) this.renderer.render(this.scene, this.camera);
+  };
+
+  render() {
+    return (
+      <div
+        style={{ width: window.innerWidth, height: window.innerHeight }}
+        ref={(mount) => {
+          this.mount = mount;
+        }}
+      />
+    );
+  }
+}
+
+export default ThreeScene;
